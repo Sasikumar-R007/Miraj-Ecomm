7f37876170dc8a06b79d4885e995d949bfffee63
Sasikumar-R007 <sasirajkumar7rs@gmail.com>
2025-08-25 21:46:23 +0530

    Revert "Assistant checkpoint: Create missing context files for admin app"
    This reverts commit ccc9a9ca70c25927f288a8d1af362c8e5ee079cd.

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,21 +2,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext'
-import { LoadingProvider } from './context/LoadingContext'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoadingProvider>
-        <AuthProvider>
-          <App />
-          <Toaster position="top-right" />
-        </AuthProvider>
-      </LoadingProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
